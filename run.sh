f450b88db3dd8508c98db97ea8b29f28d218110c
konsmosc <konsmosc@iti.gr>
2021-05-13 11:06:27 +0300

    fix issues in yaml

--- run.sh
+++ run.sh
@@ -3,6 +3,30 @@
 echo "Starting docker ."
 docker-compose up -d --build
 
+sleep 5
+echo -ne "\n\nWaiting for the systems to be ready.."
+function test_systems_available {
+  COUNTER=0
+  until $(curl --output /dev/null --silent --head --fail http://localhost:$1); do
+      printf '.'
+      sleep 2
+      let COUNTER+=1
+      if [[ $COUNTER -gt 30 ]]; then
+        MSG="\nWARNING: Could not reach configured kafka system on http://localhost:$1 \nNote: This script requires curl.\n"
+
+          if [[ "$OSTYPE" == "darwin"* ]]; then
+            MSG+="\nIf using OSX please try reconfiguring Docker and increasing RAM and CPU. Then restart and try again.\n\n"
+          fi
+
+        echo -e $MSG
+        clean_up "$MSG"
+        exit 1
+      fi
+  done
+}
+
+test_systems_available 8083
+
 echo -e "\nConfiguring the MongoDB ReplicaSet.\n"
 docker-compose exec mongo1 /usr/bin/mongo --eval '''if (rs.status()["ok"] == 0) {
     rsconf = {
@@ -24,7 +48,7 @@ curl -X GET "http://localhost:8083/connectors/" -w "\n"
 
 sleep 5
 
-echo -e "\nAdding MongoDB Kafka Sink Connector for the 'pageviews' topic into the 'test.pageviews' collection:"
+echo -e "\nAdding MongoDB Kafka Sink Connector for the 'topic' topic into the 'test.logs' collection:"
 curl -X POST -H "Content-Type: application/json" --data '
   {"name": "mongo-sink",
    "config": {
@@ -44,7 +68,7 @@ sleep 2
 echo -e "\nKafka Connectors: \n"
 curl -X GET "http://localhost:8083/connectors/" -w "\n"
 
-echo "Looking at data in 'db.pageviews':"
+echo "Looking at data in 'db.test':"
 docker-compose exec mongo1 /usr/bin/mongo --eval 'db.test.find()'
 
 
